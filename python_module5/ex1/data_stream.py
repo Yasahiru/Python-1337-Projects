9fe5d6830688759a008b67c1ff30c789c7b5f095
hloutman <hloutman@student.1337.ma>
2026-02-27 18:06:49 +0000

    last update => Feb 27/02/2026 18:06

--- python_module5/ex1/data_stream.py
+++ python_module5/ex1/data_stream.py
@@ -6,7 +6,7 @@ class DataStream(ABC):
 
     @abstractmethod
     def process_batch(self, data_batch: List[Any]) -> str:
-        ...
+        return (data_batch)
 
     def filter_data(
         self,
@@ -20,7 +20,9 @@ class DataStream(ABC):
 
 
 class SensorStream(DataStream):
-    @abstractmethod
+    def __init__(self, stream_id: str) -> None:
+        self.stream_id = stream_id
+
     def process_batch(self, data_batch: List[Any]) -> str:
         ...
 
@@ -29,35 +31,79 @@ class SensorStream(DataStream):
         data_batch: List[Any],
         criteria: Optional[str] = None
     ) -> List[Any]:
+
+        valid_data: List[Union(int, float)] = []
         try:
             for data in data_batch:
-                if isinstance(data, Union[int, float]) is not True:
+                try:
+                    sub_data: Any = data.split(":")
+                    temp: float = float(sub_data[1])
+                    valid_data.append(temp)
+                except Exception:
                     pass
-        except Exception as e:
-            print(e)
+            return valid_data
+        except Exception:
+            return valid_data
+
+    def get_stats(self) -> Dict[str, Union[str, int, float]]:
+        data: List[float] = self.filter_data()
+
+        status: Dict[str, Union[str, int, float]] = {
+            "stream_id": self.stream_id,
+            "total_readings": len(data),
+            "avg_temperature": float,
+            "critical_alerts": 0
+        }
+        return status
 
 
 class TransactionStream(DataStream):
-    ...
+    def __init__(self, stream_id: str) -> None:
+        self.stream_id = stream_id
 
 
 class EventStream(DataStream):
-    ...
+    def __init__(self, stream_id: str) -> None:
+        self.stream_id = stream_id
 
 
-class StreamProcessor(DataStream):
-    ...
+class StreamProcessor:
+    def run(self, streams: List[DataStream]) -> None:
+        ...
 
 
-class StreamManager():
-    ...
+def data_stream_test():
+    print("=== CODE NEXUS - POLYMORPHIC STREAM SYSTEM ===")
+
+    init: List[str] = [
+        "Initializing Sensor Stream...",
+        "Initializing Transaction Stream...",
+        "Initializing Event Stream..."
+    ]
+
+    sensor_stream: SensorStream = SensorStream("SENSOR_001"),
+    sensor_transaction: TransactionStream = TransactionStream("TRANS_001"),
+    sensor_event: EventStream = EventStream("EVENT_001")
+
+    inst: List[DataStream] = [
+        sensor_stream,
+        sensor_transaction,
+        sensor_event
+    ]
+
+    vals = [
+        ["temp:22.5", "humidity:65", "pressure:1013"],
+        ["buy:100", "sell:150", "buy:75"],
+        ["login", "error", "logout"]
+    ]
+
+    for test in zip(init, inst, vals):
+        print(f"{test[0]}")
 
 
 def main():
-    DataStream
+    data_stream_test()
 
 
 if __name__ == "__main__":
     main()
-
-# SUUNY123456789@ha.
